df122f97d567886cd41b407a5f65dbc2b7d4e77e
Eric Vincent Bermudez <ev.bermudez@gmail.com>
2023-09-10 02:24:29 +0800

    Update recipeRoutes.js

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -42,4 +42,73 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id).populate('author', 'username');
+
+        if (!recipe) {
+            return res.status(404).send('Recipe not found');
+        }
+
+        res.json(recipe);
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
+router.put('/:id', auth, async (req, res) => {
+    try {
+        let recipe = await Recipe.findById(req.params.id);
+
+        if (!recipe) {
+            return res.status(404).send('Recipe not found');
+        }
+
+        // Ensure the logged-in user is the author of the recipe
+        if (recipe.author.toString() !== req.user.userId) {
+            return res.status(403).send('Not authorized to update this recipe');
+        }
+
+        const updates = Object.keys(req.body);
+        const allowedUpdates = ['title', 'ingredients', 'category']; // Add more fields as needed
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+        if (!isValidOperation) {
+            return res.status(400).send('Invalid updates!');
+        }
+
+        // Update the recipe
+        updates.forEach(update => recipe[update] = req.body[update]);
+        await recipe.save();
+
+        res.json(recipe);
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id);
+
+        if (!recipe) {
+            return res.status(404).send('Recipe not found');
+        }
+
+        // Ensure the logged-in user is the author of the recipe
+        if (recipe.author.toString() !== req.user.userId) {
+            return res.status(403).send('Not authorized to delete this recipe');
+        }
+
+        await recipe.remove();
+
+        // Optionally remove the recipe's reference from the User's createdRecipes array
+        await User.findByIdAndUpdate(req.user.userId, { $pull: { createdRecipes: recipe._id } });
+
+        res.json({ message: 'Recipe deleted' });
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
