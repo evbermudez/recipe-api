a2b904e879ff79fb155e615d3142dddac0a919d5
Eric Vincent Bermudez <ev.bermudez@gmail.com>
2023-09-10 02:31:57 +0800

    Change mongoDB connection

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_URI, { 
     useNewUrlParser: true,
     useUnifiedTopology: true 
-}, () => {
+})
+.then(() => {
     console.log('Connected to MongoDB');
+})
+.catch(error => {
+    console.error('Error connecting to MongoDB', error);
 });
 
 // Middleware
